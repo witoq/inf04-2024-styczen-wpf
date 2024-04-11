566fc273ae6424186cd21cbaa5bd07a109cb19ce
witoq <witekkupka@o2.pl>
2024-04-11 10:15:33 +0200

    egzamin inf04 styczen 2024

---
+++ egzamin_inf_04_2024_01_desktopowa/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace egzamin_inf_04_2024_01_desktopowa
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ egzamin_inf_04_2024_01_desktopowa/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace egzamin_inf_04_2024_01_desktopowa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void zmiana_zdj(object sender, RoutedEventArgs e)
+        {
+            if(int.TryParse(numer_text_box.Text ,out int a))
+            {
+                if (a == 000 || a == 111 || a == 333)
+                {
+                    zdj.Visibility= Visibility.Visible;
+                    odcisk_zdj.Visibility = Visibility.Visible;
+                    zdj.Source = new BitmapImage(new Uri("Images/" + a + "-zdjecie.jpg", UriKind.Relative));
+                    odcisk_zdj.Source = new BitmapImage(new Uri("Images/" + a + "-odcisk.jpg", UriKind.Relative));
+                }
+                else
+                {
+                    zdj.Visibility = Visibility.Hidden;
+                    odcisk_zdj.Visibility = Visibility.Hidden;
+                }
+
+            }
+        }
+        private void wyswietl_komunikat(object sender, RoutedEventArgs e)
+        {
+            string kolor_oczu = "";
+            if(niebieskie.IsChecked== true) {
+                kolor_oczu = "niebieskie";
+            }
+            if (zielone.IsChecked == true)
+            {
+                kolor_oczu = "zielone";
+            }
+            if (piwne.IsChecked == true)
+            {
+                kolor_oczu = "piwne";
+            }
+            if (imie_text_box.Text.Equals("") || nazwisko_text_box.Text.Equals(""))
+            {
+                MessageBox.Show("Wprowadź dane");
+            }
+            else
+            {
+                MessageBox.Show(imie_text_box.Text + " " + nazwisko_text_box.Text + " kolor oczu " + kolor_oczu);
+            }
+        }
+    }
+}
